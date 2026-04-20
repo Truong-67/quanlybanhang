f5f367d8c7d1f42eda10d2474584c31e4a8c3299
Truong-67 <nguyentruongcorem@gmail.com>
2026-04-20 14:57:54 +0700

    Add hangHoa to response JSON in data.ts

--- api/data.ts
+++ api/data.ts
@@ -22,6 +22,7 @@ export default async function handler(req: any, res: any) {
           giaoDich,
           khachHang,
           nhaCungCap,
+          hangHoa // 🔥 THÊM DÒNG NÀY
         });
       }
     }
