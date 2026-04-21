5e0649a597dc652d03b846badec42286a3405f49
Truong-67 <nguyentruongcorem@gmail.com>
2026-04-21 11:17:51 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "sales-app-v1";
 
+// cache khi cài
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
+});
+
+// dùng cache khi fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
